00510a7ccc7602f6b8ab01d808c547142d8cd0f5
Bagros <wifi5@centrum.cz>
2023-04-20 10:10:11 +0200

    opravil jsem head

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 
 import styles from './index.module.css';
 
