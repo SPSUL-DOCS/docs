be11421598272bb82cbca19f61f4ad5119ea2d0e
JakubGrezl <jakub.grezl@outlook.cz>
2023-09-13 20:05:35 +0200

    funguje ten graf (je to epicke)
    A POZOR, AUTOMATICKY GENEROVANI POMOCI POLE OBJEKTU OKAY, NESAHEJ MI NA TEN KOD VIDIM TE, PIS TO DO TOHO POLE SVINE

--- docs/17-CJ/timeline.js
+++ docs/17-CJ/timeline.js
@@ -1,41 +1,53 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import React from 'react';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import React from "react";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
-
-export default function Timeline({children, color}) {
+export default function Timeline() {
   const timelineElements = [
     {
+      title: "Romantismus",
+      date: "2020-01-01",
+      popis: "Lorem ipsum",
+      autori: ["karel", "vojta"],
+      url: "romantismus",
+    },
+    {
+        title: "Realismus",
+        date: "2020-01-01",
+        popis: "Lorem ipsum",
+        autori: ["karel", "vojta"],
+        url: "realismus",
+      },
+  ];
 
-    }
-  ]
-  
-    return (
+  const handleClick = (url) => {
+    // Open the URL in a new window or tab
+    window.open(url, "_self");
+  };
+
+  return (
     <VerticalTimeline>
+      {timelineElements.map((element, index) => (
         <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<AutoStoriesIcon />}
+          className="vertical-timeline-element--work"
+          date={element.date}
+          iconStyle={{ background: "rgb(206, 66, 19)", color: "#fff" }}
+          icon={<AutoStoriesIcon />}
+          iconOnClick={() => handleClick(element.url)}
         >
-        <h1>Romantismus</h1>
+          <h1 style={{ color: "rgb(206, 66, 19)" }}>{element.title}</h1>
+          <p style={{ color: "black" }}>{element.popis}</p>
+          <p style={{ color: "gray" }}>
+            autoři:{" "}
+            {element.autori.map((autor) => (
+              <span>{autor}, </span>
+            ))}
+          </p>
         </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<AutoStoriesIcon />}
-        >
-        <h1>Romantismus</h1>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            icon={<AutoStoriesIcon />}
-        >
-        <h1>Romantismus</h1>
-        </VerticalTimelineElement>
+      ))}
     </VerticalTimeline>
   );
 }
